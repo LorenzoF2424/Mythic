40fe0f8d8528aa485001a30ae6a482d359ebcdcc
ilmitico001 <ilmitico001@hotmail.com>
2025-12-08 22:24:25 +0100

    testnomorefloppy

--- System/bootloader.asm
+++ System/bootloader.asm
@@ -1,6 +1,6 @@
 bits 16
 org 0x7c00
-
+    
     ;variables initialization
     xor ax, ax
     mov ds, ax
@@ -28,21 +28,18 @@ org 0x7c00
     mov cl,2
     mov dh,0
     mov dl,[diskNum]
-    mov bx, 0x7e00
+    mov bx, 0x7E00
     int 13h
 
     mov bl,1
     call check_disk_operation_success
 
     ;protected mode loader section
-load_protected_mode:
     mov si, msg3
     call printf
 
     ;checking A20
-    in al, 0x92
-    or al, 2
-    out 0x92, al
+    call enable_A20
 
     ;loading descriptor
     cli
@@ -91,10 +88,6 @@ printf:
 ret
 
 
-
-
-
-		;Add boot signature at the end of bootloader
 check_disk_operation_success:
     jc .disk_error
     jmp .false1
@@ -125,6 +118,8 @@ check_disk_operation_success:
 
 ret
 
+
+
 diskNum db 0
 msg db "Bootloader trovato!!! Eseguendo il codice.....", 13, 10, 0
 msg2 db "Ampliando spazio di memoria dedicato al bootloader....", 13, 10, 0
@@ -143,6 +138,14 @@ DW 0xAA55
 ;=========================================================================
 
 msg3 db "Avviando modalita' protetta a 32 bit......", 13, 10, 0
+
+enable_A20:
+    pusha
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    popa
+ret
 
 GDT_Start:
     null_descriptor:
@@ -190,15 +193,26 @@ protected_mode:
     
     mov esp, 0x00090000  ; Stack sicuro
     
-    ; *** CORREZIONE: usa EDI come puntatore ***
-    mov edi, 0xB8000     ; Indirizzo base
+    ; edi = display cursor
+    ;mov byte [edi], 'A'      ; ASCII CHAR
+    ;mov byte [edi+1], 0x0F     ; Attributes (white on black)
 
-    mov byte [edi], 'A'      ; Carattere ASCII
-    mov byte [edi+1], 0x0F     ; Attributo (bianco su nero)
-    
-    ; Scrivi anche 'B' per essere sicuro
-    mov byte [edi+2], 'B'
+
+
+    mov dl,39
+    mov dh,8
+    call loadAt
+    mov byte [edi], 'C'     
+    mov byte [edi+1], 0x0F     
+    mov byte [edi+2], 'I'
     mov byte [edi+3], 0x0F
+    mov byte [edi+4], 'A'
+    mov byte [edi+5], 0x0F
+    mov byte [edi+6], 'O'
+    mov byte [edi+7], 0x0F
+
+
+ 
 
 
 
@@ -209,9 +223,40 @@ protected_mode:
 
 
 
-
-
-
-
+    ;call load_Long_Mode
 JMP $
+
+loadAt: ;converte dl in colonna e dh in riga(l*h)
+    push ebx
+    push eax
+    push ecx
+
+    mov ebx,0xB8000
+    mov eax,80
+    mul dh
+    add al, dl
+    mov cl,2
+    mul cx
+    add ebx,eax
+    mov edi,ebx
+
+    pop ecx
+    pop eax
+    pop ebx
+ret
+
+
+load_Long_Mode:
+
+    mov eax, cr0
+    and eax,0x7FFFFFFF
+    mov cr0,eax
+    mov eax, cr4
+    or eax,0x620
+    mov cr4,eax
+
+ret
+
 TIMES 1024 - ($ - $$) db 0
+
+
